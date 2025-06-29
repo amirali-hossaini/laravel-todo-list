9fa28bd92069f479c2d94231ad0853863a85891f
Amirali Hossaini <amirdev1024@gmail.com>
2025-06-29 14:18:37 +0330

    feat(users): implement controller, views and requests

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,4 +85,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Project::class);
     }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
